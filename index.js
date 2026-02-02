fe744c9ac59a271f70799ceba4277929bac56033
CompSciHigh24 <csh-github@compscihigh.org>
2026-02-02 19:46:25 +0000

    s

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const app = express();
 // Task 0: Create the public and views folders and move the correct files into place
 // Task 1: Set up the static files middleware
 
-
-// Task 2: Set up a route handler for / that sends back index.html
-
-
+app.use(express.static(__dirname + '/public'))
 
 
 // Task 3: Set the view engine to EJS. 
+
+app.set('view engine', 'ejs')
 
 
 // Task 4: Set up a route handler for /student/1 that...
@@ -20,7 +19,17 @@ const app = express();
   // "advisory" (string)
   // "imageUrl" (string)
   // "funFact" (string)
-  
+
+  app.get('/student/1', (req, res) => {
+    const student = {
+      name: 'Derquan', 
+      advisory: 'Diamond nasty', 
+      imageUrl: 'https://www.ncsasports.org/_next/image?url=https%3A%2F%2Fs3.amazonaws.com%2Frms-rmfiles-production%2Fclient_photos%2Fathlete_528630_profile.jpg&w=384&q=75', 
+      funFact: 'the shiniest diamond is the one who polishes himself'
+    }
+    res.render('student.ejs', student)
+  })
+  // <%= name %>
 
   // ...and sends back student.ejs with the data object
 
@@ -31,8 +40,6 @@ const app = express();
 // EXTRA CREDIT
 // Task 6: Try to write your own teacher.ejs template that sends back a slightly different info for a teacher (e.g. classes, department)
 // Task 7: Set up a route handlers for /teacher/1 and /teacher/2 that use your new template!
-
-
 
 
 app.listen(3000, ()=>{
